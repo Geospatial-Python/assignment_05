1b7754c018d4ccc4d3c357572f57e5bd81f6d1ca
al <al@als-MacBook-Pro.local>
2016-02-27 14:48:57 -0700

    'int' object is not subscriptable...I will figure this out

--- analytics.py
+++ analytics.py
@@ -1,4 +1,4 @@
-import utils
+from .utils import *
 
 def find_largest_city(gj):
   """
@@ -192,9 +192,9 @@ def create_random(n):
 def permutations(p=99, n=100):
 	#Compute the mean nearest neighbor distance
 	permutationz = []
-	for i in range(p):
+	for i in p:
 		points = create_random(n)
-		nearest_neighbor_distance = analytics.average_nearest_neighbor_distance(points)
+		nearest_neighbor_distance = average_nearest_neighbor_distance(points)
 		permutationz.append(nearest_neighbor_distance)
 	return permutationz
 
--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -1,11 +1,23 @@
 import os
 import sys
 import unittest
+import numpy
 sys.path.insert(0, os.path.abspath('..'))
 
 from .. import analytics
 
 class TestAnalytics(unittest.TestCase):
 
-    def setUp(self):
-        pass
+  def setUp(self):
+    pass
+
+
+  def test_compute_critical(self):
+    self.assertTrue(self.lower > .01)
+    self.assertTrue(self.upper < .10)
+
+  def test_permutations(self):
+    some_points = [1, 3, 4, 10, 3.2, 10]
+    print(some_points)
+
+    critical_points = analytics.compute_critical(some_points)
