0adc8a14515f944f4c1752bb06948dd24581d458
pkadambi <pkadambi1@gmail.com>
2016-03-02 01:36:11 -0700

    more indent fixes

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -15,12 +15,12 @@ class TestAnalytics(unittest.TestCase):
 		
 	def test_find_largest(self):
 		city, pop = analytics.find_largest_city(self.gj)
-        self.assertEqual(city, 'New York')
-        self.assertEqual(pop, 19040000)
+		self.assertEqual(city, 'New York')
+		self.assertEqual(pop, 19040000)
 
 	def test_write_your_own(self):
 		some_return = analytics.write_your_own(self.gj)
-        self.assertEqual(187,some_return)
+		self.assertEqual(187,some_return)
 		
 	def test_p_perms(self):
 		self.assertEqual(len(analytics.p_perms()),99)
@@ -31,19 +31,19 @@ class TestAnalytics(unittest.TestCase):
 
 	def test_average_nearest_neighbor_distance(self):
 		mean_d = analytics.average_nearest_neighbor_distance(self.points)
-        self.assertAlmostEqual(mean_d, 7.629178, 5)
+		self.assertAlmostEqual(mean_d, 7.629178, 5)
 
 	def test_mean_center(self):
 		x, y = analytics.mean_center(self.points)
-        self.assertEqual(x, 47.52)
-        self.assertEqual(y, 45.14)
+		self.assertEqual(x, 47.52)
+		self.assertEqual(y, 45.14)
 		
 	def test_minimum_bounding_rectangle(self):
 		mbr = analytics.minimum_bounding_rectangle(self.points)
-        self.assertEqual(mbr, [0,0,94,98])
+		self.assertEqual(mbr, [0,0,94,98])
 
 	def test_expected_distance(self):
 		area = 9212
-        npoints = 50
-        expected = analytics.expected_distance(area, npoints)
-        self.assertAlmostEqual(expected, 6.7867518, 5)		
+		npoints = 50
+		expected = analytics.expected_distance(area, npoints)
+		self.assertAlmostEqual(expected, 6.7867518, 5)		
