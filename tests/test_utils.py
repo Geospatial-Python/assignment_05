bc979c02cbf874e1bff946a5e9aa8b94ce7e3751
erico637 <efriesen@asu.edu>
2016-03-01 21:13:50 -0700

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,15 +5,17 @@ sys.path.insert(0, os.path.abspath('..'))
 
 from .. import utils
 
+
 class TestUtils(unittest.TestCase):
 
     def setUp(self):
-       self.rand_points = utils.create_random(100)
-       self.permutations = utils.permutations(99)
 
-     def test_create_random(self):
-     self.assertEqual(100, len(self.rand_points))
+         self.rand_points = utils.create_random(100)
+         self.permutations = utils.permutations(99)
 
-     def test_permutations(self):
+    def test_create_random(self):
+         self.assertEqual(100, len(self.rand_points))
+
+    def test_permutations(self):
          self.assertEqual(len(self.permutations), 99)
          self.assertNotEqual(self.permutations[0], self.permutations[1])
