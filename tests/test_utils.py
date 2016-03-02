ecc75ab3f9e99c1ce2890fc71f2c71efdb4d6dc7
erico637 <efriesen@asu.edu>
2016-03-01 20:49:02 -0700

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,4 +8,12 @@ from .. import utils
 class TestUtils(unittest.TestCase):
 
     def setUp(self):
-        pass
+       self.rand_points = utils.create_random(100)
+       self.permutations = utils.permutations(99)
+
+     def test_create_random(self):
+     self.assertEqual(100, len(self.rand_points))
+
+     def test_permutations(self):
+         self.assertEqual(len(self.permutations), 99)
+         self.assertNotEqual(self.permutations[0], self.permutations[1])
